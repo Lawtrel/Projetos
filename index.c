36e75842c62443386da3336a512ec31c4a69c014
Lawtrel <kazemarus@outlook.com>
2023-09-09 02:11:12 -0300

    criando site de gatos

--- index.c
+++ index.c
@@ -1,6 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+char nome[20];
+int anos;
 
 int main(){
-    printf("oi");
+    setlocale(LC_ALL,"Portuguese");
+
+    printf("qual o seu nome? \n");
+    scanf("%s",&nome);
+    printf("qual a sua idade? \n");
+    scanf("%d",&anos);
+    
+    printf("Seu nome é %s e sua idade é %d",nome,anos);
+    
     return 0;
 }
